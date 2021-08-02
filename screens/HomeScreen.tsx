d3ec24098f5a9a8f2a314d9c16cbeeb470fce9a8
Ref <matlapengrp@gmail.com>
2021-08-02 15:15:35 +0200

    Update

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -10,7 +10,6 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title} category='h2'>Tab Home</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
